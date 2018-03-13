2f47f8f052a2a56f418d8882b486d0cd42ece2d1
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-03-12 20:10:16 -0400

    team pics were blocked; updated script to download hex color from team page

--- download_team_banners.py
+++ download_team_banners.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib import urlretrieve
 import pandas as pd
+import numpy as np
 import string
+import re
 
 #set up urls of interest
 base_url = 'https://www.ncaa.com'
@@ -13,15 +15,15 @@ day_urls = [str(x) for x in range(13, 17)]
 school_df_list = []
 for day in day_urls:
 	url = base_url + score_url + day
-	
+	#
 	#scrape html
 	r = requests.get(url)
 	soup = BeautifulSoup(r.text, "html5lib")
-	
+	#
 	#get team urls and names
 	schools = soup.select('.team a')
 	school_dict = {x['title']: base_url + x['href'] for x in schools}
-	
+	#
 	#convert data into 2 column df with title and team url
 	school_df_i = pd.DataFrame(school_dict.items())
 	#add to list
@@ -32,23 +34,22 @@ school_df = pd.concat(school_df_list, ignore_index=True)
 #rename
 school_df.columns = ['name', 'url_ext']
 
-#function to convert school name to jpg file name
-def create_file_name(school):
-	clean = str(school).translate(None, string.punctuation + ' ')
-	return clean + '.jpg'
-
-#convert school name to jpg file name
-school_df['team_jpg'] = school_df['name'].apply(create_file_name)
-
-#iterate over rows to download team logos
-for i, row in school_df.iterrows():
-	#scrape html
-	r = requests.get(row['url_ext'])
+#function to get teams color from their NCAA page
+def get_team_color(url):
+	r = requests.get(url)
 	soup = BeautifulSoup(r.text, "html5lib")
-
+	
+	#try to extract hex color from tile on page
+	#na if error
 	try:
-		#extract logo url and save to file
-		logo_url = soup.select('.large > img')[0]['src']
-		urlretrieve(logo_url, 'team_banners/{}'.format(row['team_jpg']))
+		color_str = soup.select('.heading .tile-icon')[0]['style']
+		color_out = re.findall(r'#.{6}', color_str)[0]
 	except:
-		print('failed to retrieve from {}'.format(row['url_ext']))
+		color_out = np.nan
+	return color_out
+
+#get school colors
+school_df['color'] = school_df['url_ext'].apply(get_team_color)
+
+#save to file
+school_df.to_csv('data/school_color_df.csv')
